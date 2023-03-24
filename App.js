7da75a88ba06e3b61e8ac7cf61f3f89c537b9248
Jonathan Franks Oliveira <jonathan.franks011@gmail.com>
2023-03-24 15:50:26 -0300

    #

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './src/telas/Home'
+import { SafeAreaView } from 'react-native';
+
+function App(){
+
+  return <>
+  <SafeAreaView><Home/></SafeAreaView>
+  </>
+
+}
+
+export default App;
